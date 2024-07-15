07f3865cb61d39f29669af12ba249a080e336769
Michael Loistl <michael.loistl@gmail.com>
2024-07-15 11:11:12 +0100

    Add Pushkit (VoIP) Support

--- lib/apnotic/notification.rb
+++ lib/apnotic/notification.rb
@@ -1,11 +1,10 @@
 require 'apnotic/abstract_notification'
 
 module Apnotic
-
   class Notification < AbstractNotification
-    attr_accessor :alert, :badge, :sound, :content_available, :category, :custom_payload, :url_args, :mutable_content, :thread_id
-    attr_accessor :target_content_id, :interruption_level, :relevance_score
-    attr_accessor :stale_date, :content_state, :timestamp, :event, :dismissal_date
+    attr_accessor :alert, :badge, :sound, :content_available, :category, :custom_payload, :url_args, :mutable_content,
+                  :thread_id, :custom_headers, :target_content_id, :interruption_level, :relevance_score, :stale_date,
+                  :content_state, :timestamp, :event, :dismissal_date
 
     def background_notification?
       aps.count == 1 && aps.key?('content-available') && aps['content-available'] == 1
@@ -15,10 +14,10 @@ module Apnotic
 
     def aps
       {}.tap do |result|
-        result.merge!(alert: alert) if alert
-        result.merge!(badge: badge) if badge
-        result.merge!(sound: sound) if sound
-        result.merge!(category: category) if category
+        result.merge!(alert:) if alert
+        result.merge!(badge:) if badge
+        result.merge!(sound:) if sound
+        result.merge!(category:) if category
         result.merge!('content-available' => content_available) if content_available
         result.merge!('url-args' => url_args) if url_args
         result.merge!('mutable-content' => mutable_content) if mutable_content
@@ -35,7 +34,7 @@ module Apnotic
     end
 
     def to_hash
-      { aps: aps }.tap do |result|
+      { aps: }.tap do |result|
         result.merge!(custom_payload) if custom_payload
       end
     end
--- spec/apnotic/request_spec.rb
+++ spec/apnotic/request_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe Apnotic::Request do
   let(:request) { Apnotic::Request.new(notification) }
 
-  describe ".new" do
-    let(:notification) { Apnotic::Notification.new("phone-token") }
+  describe '.new' do
+    let(:notification) { Apnotic::Notification.new('phone-token') }
     let(:request) { Apnotic::Request.new(notification) }
     let(:headers) { double(:headers) }
     let(:body) { double(:body) }
@@ -14,21 +14,21 @@ describe Apnotic::Request do
       allow(notification).to receive(:body) { body }
     end
 
-    it "initializes a response with the correct attributes" do
-      expect(request.path).to eq "/3/device/phone-token"
+    it 'initializes a response with the correct attributes' do
+      expect(request.path).to eq '/3/device/phone-token'
       expect(request.headers).to eq headers
       expect(request.body).to eq body
     end
   end
 
-  describe "#build_headers_for" do
+  describe '#build_headers_for' do
     let(:notification) do
-      n                  = Apnotic::Notification.new("phone-token")
-      n.apns_id          = "apns-id"
-      n.expiration       = "1461491082"
-      n.priority         = "10"
-      n.topic            = "com.example.myapp"
-      n.apns_collapse_id = "collapse-id"
+      n                  = Apnotic::Notification.new('phone-token')
+      n.apns_id          = 'apns-id'
+      n.expiration       = '1461491082'
+      n.priority         = '10'
+      n.topic            = 'com.example.myapp'
+      n.apns_collapse_id = 'collapse-id'
       n
     end
 
@@ -39,16 +39,18 @@ describe Apnotic::Request do
     subject { build_headers }
 
     context "when it's an alert notification" do
-      it { is_expected.to eq (
-        {
-          "apns-id"          => "apns-id",
-          "apns-expiration"  => "1461491082",
-          "apns-priority"    => "10",
-          "apns-push-type"   => "alert",
-          "apns-topic"       => "com.example.myapp",
-          "apns-collapse-id" => "collapse-id"
-        }
-      ) }
+      it {
+        is_expected.to eq(
+          {
+            'apns-id' => 'apns-id',
+            'apns-expiration' => '1461491082',
+            'apns-priority' => '10',
+            'apns-push-type' => 'alert',
+            'apns-topic' => 'com.example.myapp',
+            'apns-collapse-id' => 'collapse-id'
+          }
+        )
+      }
     end
 
     context "when it's a background notification" do
@@ -56,16 +58,18 @@ describe Apnotic::Request do
         notification.content_available = 1
       end
 
-      it { is_expected.to eq (
-        {
-          "apns-id"          => "apns-id",
-          "apns-expiration"  => "1461491082",
-          "apns-priority"    => "10",
-          "apns-push-type"   => "background",
-          "apns-topic"       => "com.example.myapp",
-          "apns-collapse-id" => "collapse-id"
-        }
-      ) }
+      it {
+        is_expected.to eq(
+          {
+            'apns-id' => 'apns-id',
+            'apns-expiration' => '1461491082',
+            'apns-priority' => '10',
+            'apns-push-type' => 'background',
+            'apns-topic' => 'com.example.myapp',
+            'apns-collapse-id' => 'collapse-id'
+          }
+        )
+      }
     end
 
     context "when it's a live activity notification" do
@@ -73,16 +77,37 @@ describe Apnotic::Request do
         notification.push_type = 'liveactivity'
       end
 
-      it { is_expected.to eq (
-        {
-          "apns-id"          => "apns-id",
-          "apns-expiration"  => "1461491082",
-          "apns-priority"    => "10",
-          "apns-push-type"   => "liveactivity",
-          "apns-topic"       => "com.example.myapp",
-          "apns-collapse-id" => "collapse-id"
+      it {
+        is_expected.to eq(
+          {
+            'apns-id' => 'apns-id',
+            'apns-expiration' => '1461491082',
+            'apns-priority' => '10',
+            'apns-push-type' => 'liveactivity',
+            'apns-topic' => 'com.example.myapp',
+            'apns-collapse-id' => 'collapse-id'
+          }
+        )
+      }
+    end
+
+    context 'when it''s a voip notification' do
+      before do
+        notification.custom_headers = {
+          'apns-push-type' => 'voip'
         }
-      ) }
+      end
+
+      it do
+        is_expected.to eq(
+          'apns-id' => 'apns-id',
+          'apns-expiration' => '1461491082',
+          'apns-priority' => '10',
+          'apns-push-type' => 'voip',
+          'apns-topic' => 'com.example.myapp',
+          'apns-collapse-id' => 'collapse-id'
+        )
+      end
     end
   end
 end
